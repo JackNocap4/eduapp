60fd71a3f5f2ccaa386bc96c79a551a38fc77dcd
dimibald <jimmy_boixos@hotmail.com>
2025-07-31 19:01:43 +0300

    feat: add global exceptions handler

--- src/main/java/gr/aueb/cf/eduapp/core/ErrorHandler.java
+++ src/main/java/gr/aueb/cf/eduapp/core/ErrorHandler.java
@@ -1,6 +1,5 @@
 package gr.aueb.cf.eduapp.core;
 
-
 import gr.aueb.cf.eduapp.core.exceptions.*;
 import gr.aueb.cf.eduapp.dto.ResponseMessageDTO;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.naming.AuthenticationException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,10 +25,10 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(ValidationException e) {
-        log.error("Validation failed. Message={}", e.getMessage(), e);
+        log.error("validation failed. Message={}", e.getMessage(), e);
         BindingResult bindingResult = e.getBindingResult();
 
-        Map<String, String> errors = new HashMap<>();
+        Map<String , String > errors = new HashMap<>();
         for (FieldError fieldError : bindingResult.getFieldErrors()) {
             errors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
@@ -41,17 +40,19 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ResponseMessageDTO> handleConstraintViolationException(AppObjectNotFoundException e) {
         log.warn("Entity not found. Message={}", e.getMessage());
         return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)                                             // HttpCode 409
+                .status(HttpStatus.NOT_FOUND)
                 .body(new ResponseMessageDTO(e.getCode(), e.getMessage()));
     }
 
+
     @ExceptionHandler(AppObjectInvalidArgumentException.class)
     public ResponseEntity<ResponseMessageDTO> handleConstraintViolationException(AppObjectInvalidArgumentException e) {
-        log.warn("Invalid argument. Message={}", e.getMessage());
+        log.warn("Invalid Argument. Message={}", e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ResponseMessageDTO(e.getCode(), e.getMessage()));
     }
+
 
     @ExceptionHandler(AppObjectAlreadyExists.class)
     public ResponseEntity<ResponseMessageDTO> handleConstraintViolationException(AppObjectAlreadyExists e) {
@@ -63,9 +64,9 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(AppObjectNotAuthorizedException.class)
     public ResponseEntity<ResponseMessageDTO> handleConstraintViolationException(AppObjectNotAuthorizedException e, WebRequest request) {
-        log.warn("Authorization failed for URI={}. Message={}", request.getDescription(false), e.getMessage());
+        log.warn("Authorization failed for URI={}. Message={}", request.getDescription(false), e.getMessage()); // uri=/api/user/...
         return ResponseEntity
-                .status(HttpStatus.FORBIDDEN)                                     // 403
+                .status(HttpStatus.FORBIDDEN)   // 403
                 .body(new ResponseMessageDTO(e.getCode(), e.getMessage()));
     }
 
@@ -73,36 +74,48 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ResponseMessageDTO> handleConstraintViolationException(AppServerException e) {
         log.warn("Server error with message={}", e.getMessage());
         return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)                                     // 403
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ResponseMessageDTO(e.getCode(), e.getMessage()));
     }
+
 
     @ExceptionHandler(IOException.class)
     public ResponseEntity<ResponseMessageDTO> handleConstraintViolationException(IOException e) {
         log.error("File upload failed with message={}", e.getMessage());
         return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)                                     // 403
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ResponseMessageDTO("FILE_UPLOAD_FAILED", e.getMessage()));
     }
 
+
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ResponseMessageDTO> handleBadCredentialsException(BadCredentialsException e, HttpServletRequest request) {
+    public ResponseEntity<ResponseMessageDTO> handleBadCredentials(BadCredentialsException e, HttpServletRequest request) {
         log.warn("Failed login attempt for IP={}", request.getRemoteAddr());
         return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)                                     // 403
+                .status(HttpStatus.UNAUTHORIZED)
                 .body(new ResponseMessageDTO("UNAUTHORIZED", e.getMessage()));
     }
 
+
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<ResponseMessageDTO> handleBadCredentialsException(AuthenticationException e, HttpServletRequest request ) {
+    public ResponseEntity<ResponseMessageDTO> handleBadCredentials(AuthenticationException e, HttpServletRequest request) {
         log.warn("Failed login for IP={}", request.getRemoteAddr());
 
         String errorCode = switch (e.getClass().getSimpleName()) {
-            case "DisabledException" -> "ACCOUNT DISABLED";
-        }
+            case "DisabledException" -> "ACCOUNT_DISABLED";
+            case "LockedException" -> "ACCOUNT_LOCKED";
+            case "AccountExpiredException" -> "ACCOUNT_EXPIRED";
+            case "CredentialsExpiredException" -> "CREDENTIALS_EXPIRED";
+            default -> "ACCOUNT_ERROR";
+        };
 
         return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)                                     // 403
-                .body(new ResponseMessageDTO("UNAUTHORIZED", e.getMessage()));
+                .status(HttpStatus.FORBIDDEN)
+                .body(new ResponseMessageDTO(errorCode, e.getMessage()));
+    }
+
+
+
+
 
 }
